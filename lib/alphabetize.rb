4bd886a352098ad15aadbde3ac25fc1c2dae9a2f
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-17 00:34:23 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,11 @@
 def alphabetize(arr)
-  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-
-  alphabet_hash = alpha_hash(alphabet)
-  word_hash = words(arr)
-  word_hash = sorted_hash(alphabet_hash, word_hash)
-  words = sort_words(word_hash)
-end
+  result_array = []
+    esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+    alphabet_array = esperanto_alphabet.split("")
+     alphabet_array.each do |letter|
+       arr.each do |phrase|
+        if phrase[0] == letter
+          result_array << phrase
+        end
+      end
+    end
