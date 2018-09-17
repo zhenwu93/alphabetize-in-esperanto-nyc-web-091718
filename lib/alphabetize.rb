9a3ae3e7c46ede81b0bb77edfab9b0084877f5f6
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-17 00:30:39 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
-  alphabet_hash = reg_hash(alphabet)
+  alphabet_hash = alpha_hash(alphabet)
   word_hash = words(arr)
   word_hash = sorted_hash(alphabet_hash, word_hash)
   words = sort_words(word_hash)
